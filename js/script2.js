8db8318a2d2ee18f615508f66956a6071ba243d6
Vmjlol <maniezzojr@yahoo.com.br>
2025-04-07 10:17:12 -0300

    teste script

---
+++ js/script2.js
@@ -0,0 +1,22 @@
+let carregar = (localizacao, elementoId) => {
+    fetch(`./../files/ListaExterna/api/telefones.json`)
+        .then((response) => response.json())
+        .then((data) => {
+            let newHTML = ``;
+            
+            let dadosFiltrados = data.filter(item => item.localizacao === localizacao);
+
+            for (let i = 0; i < dadosFiltrados.length; i++) {
+                let dados = dadosFiltrados[i];
+
+                newHTML += `
+                    <tr>
+                        <td>${dados.numero}</td>
+                        <td>${dados.nome}</td>
+                    </tr>
+                `;
+            }
+
+            document.getElementById(elementoId).innerHTML = newHTML;
+        });
+}
